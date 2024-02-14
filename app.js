2a7154b8f5c03d54211daa0ac8e656902a98b2af
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-14 20:03:49 +0430

    adding some functionality

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const reviews = [
   {
     id: 1,
-    name: 'susan smith',
+    name: 'susan smith', 
     job: 'web developer',
     img: 'https://www.course-api.com/images/people/person-1.jpeg',
     text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",

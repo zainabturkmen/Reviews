7745a456bdac6b9e1d86caf3225ac8bcee3d9a8f
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-15 11:43:06 +0430

    adding some changes

--- app.js
+++ app.js
@@ -80,7 +80,6 @@ prevBtn.addEventListener('click', function () {
      
 // show random person  
 
-
 randomBtn.addEventListener("click", function() {
   currentItem = Math.floor(Math.random() * reviews.length);
   showPerson(currentItem);

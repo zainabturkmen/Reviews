72f29340ccd5c4551ee82483d71c2581666702f4
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-15 11:47:24 +0430

    adding some changes

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ let currentItem = 1;
 // load initial item
 window.addEventListener('DOMContentLoaded', function () {
   showPerson()
-});
+});  
     
 // show person based on item
 function showPerson() {
